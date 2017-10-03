874a53eb5308d3a7da4363a0ef65bca6aebfd3dd
t-g-j <tgj@TMP.local>
2017-10-03 12:14:20 +0200

    Butterworth filter
    Addded functions for making butterworth filter !not done!

---
+++ ex4/build/CMakeFiles/Ex4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex4.dir/src/main.cpp.o"
+  "CMakeFiles/Ex4.dir/src/visionmethods.cpp.o"
+  "Ex4.pdb"
+  "Ex4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
